3e19ceebbdb636bae149fdf52a37a963dc01cb2d
Danielfeng <danielfancy@gmail.com>
2013-06-04 16:52:06 +0800

    add call_cm_api.rb ruby call coremail api

---
+++ cmbin/call_cm_api.rb
@@ -0,0 +1,16 @@
+require 'socket'
+include Socket::Constants
+
+socket = Socket.new(AF_INET, SOCK_STREAM, 0)
+sockadd = Socket.pack_sockaddr_in( 6195, '192.168.173.174' )
+if (socket.connect( sockadd ))
+    puts "Connection OK"
+    #if ( socket.call("cmd=5&user_at_domain=admin@testcloud.cn", "language=1&style=2") != True)
+    #    puts "Login fail"
+    #else :
+    #    puts "Login OK SessionID:", socket.getResut()
+    #    socket.close()
+else
+    puts "Connection fail"
+end
+
