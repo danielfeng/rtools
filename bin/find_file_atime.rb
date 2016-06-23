ab13d780a797fe6c78ff04893dd3ff2556910c24
Danielfeng <danielfancy@gmail.com>
2016-06-23 16:53:59 +0800

    add find subdirectory file assess time

---
+++ bin/find_file_atime.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# Author : danielfeng
+# E-Mail : danielfancy@gmail.com
+
+
+files_hash = Hash.new
+Dir.glob("**/*.*") do |file_name|
+  assess_time = File.new(file_name).mtime
+  unless file_name == '.' || file_name == '..' then
+    files_hash[file_name] = assess_time
+  end
+end
+
+# Sort the hash.
+files_hash = files_hash.sort_by {|key, value| value}
+
+files_hash.each do |name, time|
+   puts "#{name} was assess at #{time}"
+end
+
+
