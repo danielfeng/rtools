12382c6294dba2f7c3614e2d558228292ce5bf98
Danielfeng <danielfancy@gmail.com>
2016-06-24 13:09:57 +0800

    update input size * 1000

--- bin/find_file_atime.rb
+++ bin/find_file_atime.rb
@@ -2,13 +2,18 @@
 # Author : danielfeng
 # E-Mail : danielfancy@gmail.com
 
+if ARGV.empty? 
+  puts "Please enter the file size MB"
+  return
+end
 
 files_hash = Hash.new
+file_size = ARGV[0].to_i * 1000
 
 Dir.glob("**/*.*") do |file_name|
   assess_time = File.new(file_name).atime
   unless file_name == '.' || file_name == '..' then
-    files_hash[file_name] = assess_time if File.size(file_name) > 100000000
+    files_hash[file_name] = assess_time if File.size(file_name) > file_size 
   end
 end
 
